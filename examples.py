c3580edc9778b08a15b7f0526409d3746259c34f
Binh Ng <web.bioinfo@gmail.com>
2020-02-08 23:09:57 +0700

    minor update on read_serial

--- examples.py
+++ examples.py
@@ -13,6 +13,6 @@ ser = serial.Serial(port='/dev/ttyUSB0')
 zh03b.send_cmd(ser, zh03b.get_cmd('upload'))
 
 # read PMs from ZH03B sensor
-pm1, pm2_5, pm10 = zh03b.read_pms_upload(zh03b.read_raw_serial(ser))
+pm1, pm2_5, pm10 = zh03b.read_pms_upload(zh03b.read_serial(ser))
 print(time.ctime())
 print(f'PM1: {pm1}, PM2.5: {pm2_5}, PM10: {pm10} in ug/m3')
